78b065c18b2de8587567541cae4af9c3abe2a192
Cà Phê Sữa Đá <113408584+vanw-is-me@users.noreply.github.com>
2023-05-21 14:10:51 +0700

    test push

--- src/main.js
+++ src/main.js
@@ -11,11 +11,11 @@ import App from './App.vue'
 import router from './router'
 import i18n from './utilities/i18n'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUserSecret, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faUserSecret, faMagnifyingGlass, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 const app = createApp(App)
 
-library.add(faUserSecret, faMagnifyingGlass) //đây là ví dụ về dùng FA nha Văn
+library.add(faUserSecret, faMagnifyingGlass, faEye, faEyeSlash) //đây là ví dụ về dùng FA nha Văn
 
 // nơi khai báo các package ở phạm vi global
 window.functionCustom = functionCustom
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import EmployeeInformationView from '../views/EmpInfoView.vue'
 import PageNotFound from '../views/PageNotFound.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +18,16 @@ const router = createRouter({
       meta: {
         // middleware:
       }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/infoEmp/:id',
+      name: 'empinfo',
+      component: EmployeeInformationView
     }
   ]
 })
