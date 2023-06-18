8f37ef40bffdb8ad65d2e60e37d42b28c222f7fc
Dung <dunghq2110@gmail.com>
2023-06-18 14:36:32 +0700

    set up firebase

--- src/API.js
+++ src/API.js
@@ -2,12 +2,22 @@ import axios from "axios"
 
 export default class API {
     static BASE_URL_V1 = 'https://localhost:5001'
+    static ALLOWANCE = '/allowance'
 
-    static login(loginData){
+    // auth
+    static login(loginData) {
         return axios.post(`${this.BASE_URL_V1}/login`, loginData)
     }
 
+
+    // department
     static getListDepartment() {
         return axios.get(`${this.BASE_URL_V1}/department`)
     }
+
+    // allowance
+    static getListAllowance() {
+        return axios.get(`${this.BASE_URL_V1}/${this.ALLOWANCE}/GetListAllowance`)
+    }
+
 }
