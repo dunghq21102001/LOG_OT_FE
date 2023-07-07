3d17c736b070afa47dbcaecd391cd0719ebf39c6
Dung <dunghq2110@gmail.com>
2023-07-08 01:39:22 +0700

    dev done create emp

--- src/API.js
+++ src/API.js
@@ -47,6 +47,9 @@ export default class API {
     static getContractList(page) {
         return axios.get(`${this.BASE_URL_V1}/${this.CONTRACT}/GetList?pg=${page}`)
     }
+    static createEmployeeContract(data) {
+        return axios.post(`${this.BASE_URL_V1}/${this.CONTRACT}/Create`, data)
+    }
 
     //employee
     static getListEmployee(page = 1) {
@@ -109,6 +112,17 @@ export default class API {
 
     static updateLevel(id, data) {
         return axios.put(`${this.BASE_URL_V1}/Level/Update/${id}`, data);
+    }
+
+    //enums
+    static salaryType() {
+        return axios.get(`${this.BASE_URL_V1}/api/Enum/SalaryType`)
+    }
+    static contractType() {
+        return axios.get(`${this.BASE_URL_V1}/api/Enum/ContractType`)
+    }
+    static insuranceType() {
+        return axios.get(`${this.BASE_URL_V1}/api/Enum/InsuranceType`)
     }
 
 }
